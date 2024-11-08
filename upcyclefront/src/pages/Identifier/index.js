a06f6f774d3538d738db1bdc91342ac0d7956dfe
paulo <paulo.m.campos6601@gmail.com>
2024-11-07 22:40:49 -0300

    add: create identifier page
    Co-authored-by: Estela Lacerda <elo@cesar.school>
    Co-authored-by: Gabriel Rossiter <capv2004@gmail.com>

--- upcyclefront/src/pages/Identifier/index.js
+++ upcyclefront/src/pages/Identifier/index.js
@@ -1,19 +1,23 @@
 import styles from './Identifier.module.css';
 
 function Identifier() {
-    return(
+    return (
         <>
-            <div className={styles['identifier-body']}>
-                <h1 className={styles.question}>
-                    ?
-                </h1>
-                <h2 className={styles.subtitle}>
-                    Gostaria de se identificar?
-                </h2>
-                <div className={styles['button-container']}>
-                    <button className={styles['yes-button']}>SIM</button>
-                    <button className={styles['no-button']}>NÃO</button>
+            <div className={styles['identifier-container']}>
+                <div className={styles['identifier-body']}>
+                    <img className={styles['identifier-logo']} src='/images/logo.svg' alt='Logo do Banco do Brasil' />
+                    <p className={styles.subtitle}>
+                        <strong>Bem vindo</strong> ao descarte eletrônico do BB!
+                    </p>
+                    <p className={styles.question}>
+                        Você gostria de se identificar?
+                    </p>
+                    <div className={styles['button-container']}>
+                        <button className={styles['yes-button']}>Sim</button>
+                        <button className={styles['no-button']}>Não</button>
+                    </div>
                 </div>
+                <div className={styles['identifier-image']}><img src='./images/recycle.png' alt='Símbolo da reciclagem em tons de azul' /></div>
             </div>
         </>
     );
